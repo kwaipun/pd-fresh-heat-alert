c522afa553c206dfe50496ba3577364823fc3240
Christopher Kwai-Pun <chris.kwaipun@gmail.com>
2017-09-16 11:08:08 -0400

    spacing

--- heat_alert.py
+++ heat_alert.py
@@ -6,39 +6,42 @@ from datetime import datetime
 from os import environ
 
 pypd.api_key = environ.get('pd_api_key')
-reddit = praw.Reddit(client_id=environ.get('reddit_cid'), client_secret=environ.get('reddit_secret'),
+reddit = praw.Reddit(client_id=environ.get('reddit_cid'),
+                     client_secret=environ.get('reddit_secret'),
                      user_agent='testscript')
 
 subreddit = reddit.subreddit('hiphopheads')
 
 viewed = []
 
+
 def clean(item):
     diff = datetime.now() - item['timestamp']
     if diff.days == 0:
         return True
 
+
 def fetch():
-    for submission in subreddit.hot(limit=25):
-        if submission.title.startswith('[FRESH') and submission.id not in viewed:
-            viewed.append({'title': submission.title,
-                            'url': submission.url,
-                            'timestamp': datetime.fromtimestamp(submission.created)})
+    for s in subreddit.hot(limit=25):
+        if s.title.startswith('[FRESH') and s.id not in viewed:
+            viewed.append({'title': s.title,
+                           'url': s.url,
+                           'timestamp': datetime.fromtimestamp(s.created)})
 
             pypd.Event.create(data={
                 'service_key': environ.get('pd_service_key'),
                 'event_type': 'trigger',
-                'description': submission.title,
+                'description': s.title,
                 'contexts': [
                     {
                         'type': 'link',
-                        'href': submission.url,
+                        'href': s.url,
                         'text': 'FRESH HEAT ALERT',
                     },
-                            ],
+                ],
             })
 
-            print (submission.title)
+            print(s.title)
 
 
 while True:
